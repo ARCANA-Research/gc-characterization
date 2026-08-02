136da6fe04c24f08e95eb14b6073fe50a12226b5
sudhanshu2 <24910339+sudhanshu2@users.noreply.github.com>
2026-08-01 23:09:30 -0500

    Remove `--revision` flag and update Ramulator2 base commit

--- setup.sh
+++ setup.sh
@@ -35,7 +35,8 @@ fi
 
 if [ ! -d "$RUNNING_NG_DIR" ]; then
   echo "Setup running-ng"
-  git clone --revision=53b4be62b48e00fceab6308cdc8c4d4cf7e10000 https://github.com/anupli/running-ng.git $RUNNING_NG_DIR
+  git clone https://github.com/anupli/running-ng.git $RUNNING_NG_DIR
+  git -C $RUNNING_NG_DIR checkout 53b4be62b48e00fceab6308cdc8c4d4cf7e10000
   git -C $RUNNING_NG_DIR apply $PATCHES_DIR/running-ng.patch
   source $VENV_DIR/bin/activate
   pip install $RUNNING_NG_DIR
@@ -62,7 +63,8 @@ fi
 
 if [ ! -d "$JDK21_COUNTER_DIR" ]; then
   echo "Setup counter JDK21"
-  git clone --revision=7069f193f1f8c61869fc68a36c17f3a9a7b7b2a0 https://github.com/openjdk/jdk21u.git $JDK21_COUNTER_DIR
+  git clone https://github.com/openjdk/jdk21u.git $JDK21_COUNTER_DIR
+  git -C $JDK21_COUNTER_DIR checkout 7069f193f1f8c61869fc68a36c17f3a9a7b7b2a0
   git -C $JDK21_COUNTER_DIR apply $PATCHES_DIR/jdk21u-counter.patch
   env --chdir=$JDK21_COUNTER_DIR bash configure --with-jvm-variants=server \
     --with-boot-jdk=$JDK21_BOOT_DIR \
@@ -82,7 +84,8 @@ fi
 
 if [ ! -d "$JDK21_SIMULATOR_DIR" ]; then
   echo "Setup simulator JDK21"
-  git clone --revision=7069f193f1f8c61869fc68a36c17f3a9a7b7b2a0 https://github.com/openjdk/jdk21u.git $JDK21_SIMULATOR_DIR
+  git clone https://github.com/openjdk/jdk21u.git $JDK21_SIMULATOR_DIR
+  git -C $JDK21_SIMULATOR_DIR checkout 7069f193f1f8c61869fc68a36c17f3a9a7b7b2a0
   git -C $JDK21_SIMULATOR_DIR apply $PATCHES_DIR/jdk21u-simulator.patch
   env --chdir=$JDK21_SIMULATOR_DIR bash configure --with-jvm-variants=client \
     --with-boot-jdk=$JDK21_BOOT_DIR \
@@ -103,7 +106,8 @@ if [ ! -d "$GEM5_DIR" ]; then
   echo "Setup gem5"
   git clone --revision=c9625ce9cc5b5a90a38327de5ac0e1870974af5e https://github.com/gem5/gem5.git $GEM5_DIR
   git -C $GEM5_DIR apply $PATCHES_DIR/gem5.patch
-  git clone --revision=e62c84a6f0e06566ba6e182d308434b4532068a5 https://github.com/CMU-SAFARI/ramulator2 $RAMULATOR2_DIR
+  git clone https://github.com/CMU-SAFARI/ramulator2 $RAMULATOR2_DIR
+  git -C $RAMULATOR2_DIR checkout 4a29cac924e4dabc8f84c648b653ec4d3ec53f3f
   git -C $RAMULATOR2_DIR apply --ignore-space-change --ignore-whitespace $PATCHES_DIR/ramulator2.patch
   mkdir $RAMULATOR2_DIR/build
   cd $RAMULATOR2_DIR/build
